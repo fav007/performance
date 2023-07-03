71ac10c559a68841ed2d05f0aeac3d69d8e07f6a
fav007 <besthorizon@hotmail.fr>
2023-07-03 08:39:57 +0300

    ok

--- bulletin.py
+++ bulletin.py
@@ -9,16 +9,23 @@ uploaded_file = st.file_uploader("File",type=["xlsx"])
 a = 1
 if uploaded_file is not None:
     file_name = uploaded_file.name 
+    
+        
     if "21TO" in file_name :
         a = 0
     excel_data = pd.read_excel(uploaded_file,sheet_name=None)
+    with st.expander("Informations"):
+        st.write(uploaded_file.name)
+        for sheet_name in excel_data.keys():
+            
+            st.write(sheet_name,"rows:",excel_data[sheet_name].shape[0],"columns:",excel_data[sheet_name].shape[1])
     
     with st.expander("dataframe"):
         sheet_names = list(excel_data.keys())
         sheet_name = st.selectbox("select a sheet",sheet_names)
         
         st.dataframe(excel_data[sheet_name])
-    st.write(uploaded_file.name)
+    
     
     if a == 1:
         st.metric("Assiette",millify(excel_data["IND2"]["ASSIETTE"].str.replace(",","_").astype("float").sum()))
